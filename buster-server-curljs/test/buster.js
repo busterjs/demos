17a351ce45d69b391767d0bc50f43e8976ef8c71
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-02-18 00:09:09 +0000

    add curljs example

--- buster-server-curljs/test/buster.js
+++ buster-server-curljs/test/buster.js
@@ -4,20 +4,19 @@ config["Browser tests"] = {
     environment: "browser",
     rootPath: "../",
     libs: [
-      "libs/require.js",
-      "config.js"
+      "config.js",
+      "libs/curl.js"
     ],
     sources: ["js/*.js"],
     tests: ["test/*-test.js"],
     extensions: [require('buster-amd')],
     "buster-amd": {
         pathMapper: function (path) {
-          // Removes initial slash for test file paths such as
-          // /test/strftime-test.js
-          if(path.indexOf('/') === 0){
-            return path.substr(1, path.length);
-          }
-          return path;
+          return path.
+                 // remove extension
+                 replace(/\.js$/, "").
+                 // replace leading slash with previous directory for test files
+                 replace(/^\//, "../");
         }
     }
 };
